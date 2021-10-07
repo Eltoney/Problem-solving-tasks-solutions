838824d675c8ed4f52df3469447907d66025547f
Eltoney <aliahmedeltoney@gmail.com>
2021-10-08 00:00:57 +0200

    testing

--- leetcode problems/Algorithms and data structures/Best Time to Buy and Sell Stock/Solutin.cpp
+++ leetcode problems/Algorithms and data structures/Best Time to Buy and Sell Stock/Solutin.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit = 0;
+        int profit = -1;
         int mnPriceSofar = 1e6;
         for(int i = 0 ; i < prices.size() ; i++){
             mnPriceSofar = min(mnPriceSofar , prices[i]);
